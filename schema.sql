e654ae34e8dd25fc6f1d0312841aaad509aa441b
Joseph V <joseph.vonedwins@yahoo.com>
2018-08-04 15:51:45 -0400

    populate tables with seed data in schema

--- schema.sql
+++ schema.sql
@@ -16,3 +16,9 @@ department_id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 department_name VARCHAR(30) NOT NULL,
 overhead_costs DECIMAL(10, 2) NOT NULL
 );
+
+INSERT INTO departments (product_name, department_name, price, stock_quantity)
+VALUE ("Settlers of Catan", "Games", 34.99, 50), ("iPhone X", "Electronics", 899.99, 100), ("Leather Couch", "Furniture", 499.50, 10);
+
+INSERT INTO departments (department_name, overhead_costs)
+VALUE ("Games", 24000), ("Electronics", 45000), ("Furniture", 36000);
